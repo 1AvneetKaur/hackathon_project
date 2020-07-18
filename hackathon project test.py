7132643cc8273ad44d2a21839c221193c25332e5
Avneet Kaur <60478415+1AvneetKaur@users.noreply.github.com>
2020-07-18 19:46:45 +0530

    code till now

---
+++ hackathon project test.py
@@ -0,0 +1,13 @@
+# Hackathon Project Test
+
+#PART I- Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+#IMPORTING THE DATASET
+test_set = pd.read_csv('test.csv')
+
